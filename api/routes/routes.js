82a823c1101e377cec94bd31ffe8e6e2f07e4633
Andrei Popovici <me@andreipopovici.co.uk>
2018-01-11 22:54:28 +0000

    Added default message

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 
 module.exports = app => {
+  app.get('/', (req, res) => {
+    res.send({ hello: 'World' });
+  });
   app.get(
     '/auth/google',
     passport.authenticate('google', {
